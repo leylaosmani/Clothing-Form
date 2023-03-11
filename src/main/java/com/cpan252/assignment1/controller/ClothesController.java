7b9a2c04b13f7884d9f634f1c3766033ad6f4779
Leyla <Leyla.osmani@outlook.com>
2023-03-10 22:22:12 -0500

    fixed error with drop down list

--- src/main/java/com/cpan252/assignment1/controller/ClothesController.java
+++ src/main/java/com/cpan252/assignment1/controller/ClothesController.java
@@ -2,14 +2,15 @@ package com.cpan252.assignment1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import com.cpan252.assignment1.model.Clothes.Brands;
+import org.springframework.ui.Model;
 @Controller
+public class ClothesController {
 
-public class ClothesController { 
-  @GetMapping("/clothes")
-
-public String clothes(){
-    return "clothes";
-}
+    @GetMapping("/clothes")
+    public String showClothes(Model model) {
+        model.addAttribute("brands", Brands.values());
+        return "clothes";
+    }
 
 }
